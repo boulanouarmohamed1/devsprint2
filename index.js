a0f10c1752ca65dcfe7d801f33e3b244471ac0b5
achref <na_bougroura@esi.dz>
2025-04-19 10:29:50 +0100

    achref

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+app.use(express.urlencoded({ extended: true }));
+
+
+require('dotenv').config();
+const requestRoutes = require('./routes/requestRoutes');
+
+app.use(express.json());
+// Attach route
+app.use('/requests', requestRoutes);
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/users', userRoutes);
 
 // Home route
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://achrafbougroura05:yI91MOJZtVNIYsTI@cluster0.ggqgkll.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 // Start server
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`);
 });
+
+
